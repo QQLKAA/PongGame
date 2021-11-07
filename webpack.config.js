cd3bba4fda1bbfc70f1af94d6d5ea793baf557bc
Bartłomiej Kudzia <bar.kud.11340@gmail.com>
2021-11-07 11:48:14 +0100

    Dodano prosty fontend oparty na webpacku
    - Stworzono paczkę nodejs
    - Zainstalowano webpack wraz z dev-serverem
    - Stworzono prostą stronę, aby sprawdzić czy konfiguracja jest poprawna

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./frontend/src/main.js",
+  output: {
+    filename: "bundle.js",
+    path: path.join(__dirname, "public"),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
